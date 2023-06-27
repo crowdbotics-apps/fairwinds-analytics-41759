9972edd70420846c36e773b235f4c44c5f5c58d8
Crowdbotics <team@crowdbotics.com>
2023-06-27 13:51:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fairwinds-analytics-41759",
+  url: "https://fairwinds-analytics-41759.botics.co",
+  api: "https://fairwinds-analytics-41759.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
